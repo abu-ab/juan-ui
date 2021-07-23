67a97492ba04393ce3b3981f4d8456286a2bfa56
gray <1012518214@qq.com>
2021-07-23 18:03:07 +0800

    remove app types

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -1,10 +1,9 @@
-import { App } from 'vue';
 // 导入组件，组件必须声明 name
 import Button from './src/Button.vue';
 
 // 为组件提供 install 安装方法，供按需引入
 // app为组件实例
-Button.install = function (app: App) {
+Button.install = function (app) {
   app.component(Button.name, Button);
 };
 
