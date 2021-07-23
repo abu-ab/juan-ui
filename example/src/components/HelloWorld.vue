ac8a54d2caa7d7053ba84da8d0030a32d2398d7c
gray <1012518214@qq.com>
2021-07-23 18:09:27 +0800

    update .gitignore

---
+++ example/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Documentation </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <jButton />
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  props: {
+    msg: String
+  },
+  setup() {
+    const state = reactive({ count: 0 });
+    return {
+      state
+    };
+  }
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ packages/button/index.ts
@@ -0,0 +1,11 @@
+// 导入组件，组件必须声明 name
+import Button from './src/Button.vue';
+
+// 为组件提供 install 安装方法，供按需引入
+// app为组件实例
+Button.install = function (app) {
+  app.component(Button.name, Button);
+};
+
+// 默认导出组件
+export default Button;
